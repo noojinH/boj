ec8dd16a91430e658407b76ed461d5e42c8c97ef
noojinH <dunillroud@gmail.com>
2025-03-28 12:51:43 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/25304. 영수증/영수증.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+	int X,N,a,b,sum = 0;
+	scanf("%d",&X);
+	scanf("%d",&N);
+	for(int i=0;i<N;i++){
+		scanf("%d %d",&a,&b);
+		sum += a * b;
+	}
+	if(X==sum){
+		printf("Yes");
+	}
+	else{
+		printf("No");
+	}
+	return 0;
+}
