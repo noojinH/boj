9114f57051b4c9b746a4503d0036319507768ac7
noojinH <dunillroud@gmail.com>
2025-04-08 13:53:43 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 1112 KB, Score: 30 point -BaekjoonHub

---
+++ C99/백준/Bronze/15964. 이상한 기호/이상한 기호.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int p(int z1,int z2){
+    int rsl=0;
+    rsl = z1+z2;
+    return rsl;
+}
+int m(int z1, int z2){
+    int rsl=0;
+    rsl=z1-z2;
+    return rsl;
+}
+int main(void){
+    int A=0,B=0;
+    scanf("%d %d", &A, &B);
+    printf("%d", p(A,B) * m(A,B));
+}
