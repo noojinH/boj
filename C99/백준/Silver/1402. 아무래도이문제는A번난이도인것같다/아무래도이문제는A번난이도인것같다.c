82223ca4f724301fba15dfaa2ce4dfe78e24386d
noojinH <dunillroud@gmail.com>
2025-04-09 17:44:50 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int T=0,A=0,B=0;
+    scanf("%d", &T);
+    for(int i=0;i<T;i++){
+        scanf("%d %d", &A, &B);
+        printf("yes\n");
+    }
+    return 0;
+}
