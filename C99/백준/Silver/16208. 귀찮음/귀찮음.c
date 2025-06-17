de2de04fa0e43dc6455a29e779edee38fd745544
noojinH <dunillroud@gmail.com>
2025-06-17 11:23:21 +0900

    [Silver V] Title: 귀찮음, Time: 100 ms, Memory: 4916 KB, Score: 30 point -BaekjoonHub

--- C99/백준/Silver/16208. 귀찮음/귀찮음.c
+++ C99/백준/Silver/16208. 귀찮음/귀찮음.c
@@ -1,22 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define srt short
+#define ll long long
 int _cmp(const void *p, const void *q){
-    if(*(char *)p > *(char *)q) return 1;
-    if(*(char *)p < *(char *)q) return -1;
+    if(*(int *)p > *(int *)q) return 1;
+    if(*(int *)p < *(int *)q) return -1;
     return 0;
 }
 int main(void){
-    srt n;
-    scanf("%hd", &n);
-    char a[n];
-    for(srt i=0;i<n;i++) scanf("%hhd", a+i);
-    qsort(a, n, sizeof(char), _cmp);
-    long long rsl=0;
-    for(srt el=n;el>1;el--){
-        int sum = 0;
-        for(srt i=0;i<el-1;i++) sum+= a[i];
-        rsl += sum * a[el-1];
+    int n;
+    scanf("%d", &n);
+    int *a = (int *)malloc(sizeof(int) * n);
+    for(int i=0;i<n;i++) scanf("%d", a+i);
+    qsort(a, n, sizeof(int), _cmp);
+    ll rsl=0;
+    ll sum=0;
+    for(int i=0;i<n-1;i++) sum+=a[i];
+    //printf("%d ", sum);}
+    //printf("%d ", sum);
+    for(;n>1;n--){
+        //printf("%d ", a[n-1]);
+        rsl += sum * a[n-1];
+        //printf("%lld\n", rsl);
+        sum -= a[n-2];
+        //printf("%d ", sum);
     }
     printf("%lld\n", rsl);
+    free(a);
+    a = NULL;
+    return 0;
 }
