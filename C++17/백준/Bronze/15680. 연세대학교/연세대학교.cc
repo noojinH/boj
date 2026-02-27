91e8ea9bd56c7d53bcbdd36b98b167aa85aff65a
noojinH <dunillroud@gmail.com>
2026-02-27 20:03:55 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+    if(!N) cout<<"YONSEI\n";
+    else cout<<"Leading the Way to the Future\n";
+    return 0;
+}
