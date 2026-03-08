c34cf3560c2cb8b42db4be6eafb7e630d329b3bf
noojinH <dunillroud@gmail.com>
2026-03-08 11:53:46 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int A,B,C;
+    cin >> A >> B >> C;
+    if(C==A+B) cout<<"correct!\n";
+    else cout<<"wrong!\n";
+    return 0;
+}
